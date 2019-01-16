ad224204142572147981ce4270bf94bb1c13c551
Nathan Alleyne <nathan.alleyne7@gmail.com>
2019-01-16 22:55:31 +0000

    Started editing all my code for playlist

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Sunflower", "Monster", "MAMA", "Get You"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 

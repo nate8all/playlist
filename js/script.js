3beefcc86d2aa5c2c5ef206831398e9a67ab59b4
Nathan Alleyne <nathan.alleyne7@gmail.com>
2019-03-13 21:57:51 +0000

    Made new

--- js/script.js
+++ js/script.js
@@ -1,34 +1,54 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = ["Sunflower", "Monster", "MAMA", "Get You", "Middle Child"];
-// BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
-// Make sure they match the same order as your array above
-var img_links = ["https://t2.genius.com/unsafe/300x300/https%3A%2F%2Fimages.genius.com%2Ff8df6f94846dc4a8f2e1b94d7abd1cb5.1000x1000x1.jpg",
-    "https://images.rapgenius.com/6a89651a53ebadf375d20b33f412b087.500x438x1.jpg", 
-    "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/6ix9ine_%E2%80%93_Dummy_Boy.png/220px-6ix9ine_%E2%80%93_Dummy_Boy.png",
-    "https://i.ytimg.com/vi/EQWKdwvXh-o/maxresdefault.jpg", "https://static.stereogum.com/uploads/2019/01/Middle-Child-1548260765-640x640.jpg"];
-var artists = ["Swae Lee, Post Malone", "Kanye West, Jay-Z, Nicki Minaj, Rick Ross", "6ix9ine, Kanye West, Nicki Minaj", 
-    "Daniel Caesar, Kali Uchi", "J. Cole"];
-var song_lengths = ["2:38","6:10","3:10","2:38","3:33"];
-var links = ["https://www.youtube.com/watch?v=ApXoWvfEYVU", "https://www.youtube.com/watch?v=Idz9aYXq98U", 
-    "https://www.youtube.com/watch?v=gJ-bD1pU7hM", "https://www.youtube.com/watch?v=uQFVqltOXRg", "https://www.youtube.com/watch?v=e8CLsYzE5wk"];
 
+var songs = [
+    {
+        name: "Sunflower",
+        image: "https://t2.genius.com/unsafe/300x300/https%3A%2F%2Fimages.genius.com%2Ff8df6f94846dc4a8f2e1b94d7abd1cb5.1000x1000x1.jpg",
+        artist: "Swae Lee, Post Malone",
+        songlength: "2:38",
+        link: "https://www.youtube.com/watch?v=ApXoWvfEYVU" },
+    {
+        name: "Monster",  
+        image: "https://images.rapgenius.com/6a89651a53ebadf375d20b33f412b087.500x438x1.jpg",
+        artist: "Kanye West, Jay-Z, Nicki Minaj, Rick Ross",
+        songlength: "6:10",
+        link: "https://www.youtube.com/watch?v=Idz9aYXq98U" },
+    {
+        name: "MAMA",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/6ix9ine_%E2%80%93_Dummy_Boy.png/220px-6ix9ine_%E2%80%93_Dummy_Boy.png",
+        artist: "6ix9ine, Kanye West, Nicki Minaj",
+        songlength: "3:10",
+        link: "https://www.youtube.com/watch?v=gJ-bD1pU7hM" },
+    {
+        name: "Get You",
+        image: "https://i.ytimg.com/vi/EQWKdwvXh-o/maxresdefault.jpg",
+        artist: "Daniel Caesar, Kali Uchi",
+        songlength: "2:38",
+        link: "https://www.youtube.com/watch?v=uQFVqltOXRg" },
+    {
+        name: "Middle Child",
+        image: "https://static.stereogum.com/uploads/2019/01/Middle-Child-1548260765-640x640.jpg",
+        artist: "J. Cole",
+        songlength: "3:33",
+        link: "https://www.youtube.com/watch?v=e8CLsYzE5wk" }
+    ];
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
     songs.forEach(function(song){
-    $("#songs").append("<p>" + song + "</p>");
+    $("#songs").append("<p>" + song.name + "</p>");
  });
-    img_links.forEach(function(image){
-    $("#images").append("<img src=" + image + ">");
+    songs.forEach(function(image){
+    $("#images").append("<img src=" + image.image + ">");
  });
-    artists.forEach(function(artistspersong){
-    $("#artists").append("<p>" + artistspersong + "</p>");
+    songs.forEach(function(artistspersong){
+    $("#artists").append("<p>" + songs.artist + "</p>");
  });
-    song_lengths.forEach(function(songlength){
-    $("#lengths").append("<p>" + songlength + "</p>");
+    songs.forEach(function(length){
+    $("#lengths").append("<p>" + length.songlength + "</p>");
  });
-    links.forEach(function(link){
+    songs.forEach(function(link){
     $("#links").append("<p>" + "<a href=" + link + ">" + "Listen!" + "</a>" + "</p>");
  });
 }
@@ -45,18 +65,16 @@ function emptySongInfo(){
 
 function addSongInfo(){
     // BELOW write the code to add new items to each of the arrays.
-    var newSong = $("#song").val();
-    var newArtist = $("#artist").val();
-    var newSongLength = $("#length").val();
-    var newImage = $("#image").val();
-    var newLink = $("link").val();
+    var newObj = {
+        name: $("#song").val(),
+        image: $("#images").val(),
+        artist: $("#artist").val(),
+        songlength: $("#lengths").val(),
+        link: $("#link").val()
+    }
 
-    songs.push(newSong);
-    artists.push(newArtist);
-    song_lengths.push(newSongLength);
-    img_links.push(newImage);
-    links.push(newLink);
-    
+    songs.push(newObj);
+
     $("#add").click(function() {
        $(newSong).append("#songs");
        $(newArtist).append("#artists");
